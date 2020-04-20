ec65fab4779273dc552509571b718758be8223b1
raboija <raboijaofficial@gmail.com>
2020-04-20 14:08:45 +0000

    Updates

---
+++ precache-manifest.0c7713f7f9468a2d4439d01a693dad60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0554b95b058e1d3646d3adbc683fdd2",
+    "url": "/COVID19_Stats_CoronaTracker/index.html"
+  },
+  {
+    "revision": "849e128faeaf4b017556",
+    "url": "/COVID19_Stats_CoronaTracker/static/css/main.0c37df87.chunk.css"
+  },
+  {
+    "revision": "813aa18fdf6fde14dd3e",
+    "url": "/COVID19_Stats_CoronaTracker/static/js/2.c916165a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19_Stats_CoronaTracker/static/js/2.c916165a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849e128faeaf4b017556",
+    "url": "/COVID19_Stats_CoronaTracker/static/js/main.41e4ed98.chunk.js"
+  },
+  {
+    "revision": "587d726443097ddd1933",
+    "url": "/COVID19_Stats_CoronaTracker/static/js/runtime-main.6fc0b5c9.js"
+  },
+  {
+    "revision": "ac1333d5c72d2c03db8ebeb69dd46ac8",
+    "url": "/COVID19_Stats_CoronaTracker/static/media/image.ac1333d5.png"
+  }
+]);
